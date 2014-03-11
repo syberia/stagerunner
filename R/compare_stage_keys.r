468fbfbd48efa56eeed0f77a4d46ba7e529648eb
RobertZK <technoguyrob@gmail.com>
2014-03-11 00:38:26 -0500

    simplify compare_stage_keys

--- R/compare_stage_keys.r
+++ R/compare_stage_keys.r
@@ -5,26 +5,6 @@
 #' @return logical. Whether or not key1 runs a stage before key2.
 compare_stage_keys <- function(key1, key2) {
   if (length(key1) == 0) return(TRUE)
-  if (identical(key1, FALSE)) return(TRUE)
-  if (identical(key2, FALSE)) return(FALSE)
-  if (identical(key1, TRUE)) return(TRUE)
-  if (identical(key2, TRUE))
-    return(if (identical(key1[[1]], TRUE)) TRUE else compare_stage_keys(key1[[1]], key2))
-  stopifnot(length(key1) == length(key2))
-  if (length(key1) == 0) return(TRUE)
-  ix <- vapply(list(key1, key2),
-               function(el) which(vapply(el, contains_true, logical(1)))[1],
-               numeric(1))
-  stopifnot(is.finite(ix[1]) && is.finite(ix[2]))
-  if (ix[1] == ix[2]) {
-    if (is.atomic(key1) && is.atomic(key2)) return(TRUE) # tie!
-    compare_stage_keys(key1[[ix[1]]], key2[[ix[2]]])
-  }
-  else ix[1] < ix[2]
-}
-
-compare_stage_keys <- function(key1, key2) {
-  if (length(key1) == 0) return(TRUE)
   if (identical(key1, TRUE)) return(TRUE)
   ix <- vapply(list(key1, key2),
                function(el) which(vapply(el, contains_true, logical(1)))[1],
