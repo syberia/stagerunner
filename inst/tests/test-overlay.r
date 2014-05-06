a629326172e914dbccee16d6850c48f68da2bb31
RobertZK <technoguyrob@gmail.com>
2014-05-06 08:48:56 -0500

    add tests for overlay

--- inst/tests/test-overlay.r
+++ inst/tests/test-overlay.r
@@ -1,7 +1,20 @@
 context('stageRunner overlaying')
 
-test_that('it can overlay a complicated example correctly', {
-  #sr1 <- stageRunner$new(cx <- new.env(), list(c = function(x) x$x <- -1, a = function(x) { x$x <- 1; cat('setting x'); }, b = function(x) { x$x <- 2; cat('setting x to 2') }), remember = T)
-  #sr2 <- stageRunner$new(cx, list(a = list(function(x) cat('..x is ', x$x), function(x) { cat('X WAS', cached_env$x)}), b = function(x) cat('..done!')))
-  #sr1$overlay(sr2); sr1$run()
+test_that('it can overlay a simple example correctly', {
+  sr1 <- stageRunner$new(cx <- new.env(), list(a = function(x) x$x <- 1, b = function(y) x$x <- 3))
+  sr2 <- stageRunner$new(cx, list(a = function(y) y$x <- 2))
+  sr1$overlay(sr2)
+  sr1$run(1)
+  expect_identical(cx$x, 2)
+
+  # Check that reverse order works as well.
+  sr1 <- stageRunner$new(cx <- new.env(), list(a = function(x) x$x <- 1, b = function(x) x$x <- 3))
+  sr2 <- stageRunner$new(cx, list(a = function(y) y$x <- 2, b = function(x) x$x <- 3))
+  sr2$overlay(sr1)
+  sr1$run(1)
+  expect_identical(cx$x, 1)
+})
+
+test_that('it can give labels to overlays', {
+  
 })
