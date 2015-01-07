bf456145329b6171a4b2173d5d4eea312d0f7012
RobertZK <technoguyrob@gmail.com>
2015-01-07 09:29:51 -0600

    foreshadow usage of tracked_environment in test

---
+++ inst/tests/test-tracked_environment.R
@@ -0,0 +1,13 @@
+context('tracked_environment')
+
+# The objectdiff package provides the ability to use a single environment
+# to store a sequence of changes, with a Git-like ability to "roll back"
+# to previous versions of the environment. It does this without extraneous
+# memory overhead using clever heuristics.
+
+# If a stageRunner is provided a tracked_environment instead of a regular
+# environment, it will assume this behavior into its functioning so that
+# if its remember parameter is set to TRUE, it will use the tracked_environment's
+# rollback abilities to provide the correct behavior when executing an
+# earlier stage.
+
