bebf4e042854ef9fe06eeff074cf64fe14078aa4
RobertZK <technoguyrob@gmail.com>
2015-01-13 23:44:10 -0600

    test that it does not coalesce when no names overlap

--- inst/tests/test-coalescing.R
+++ inst/tests/test-coalescing.R
@@ -15,6 +15,16 @@ describe('with regular environments', {
     assert(sr2$run(2))
     expect_identical(sr2$context$y, 4)
   })
+
+  test_that("it does not coalesce when no names overlap", {
+    sr1 <- stageRunner$new(new.env(), remember = TRUE,
+      list(a = function(e) e$x <- 1, b = function(e) e$y <- 2))
+    sr2 <- stageRunner$new(new.env(), remember = TRUE,
+      list(c = function(e) e$x <- 1, d = function(e) e$y <- 4))
+    sr1$run()
+    sr2$coalesce(sr1)
+    expect_error(sr2$run(2), "some previous stages have not been executed")
+  })
 })
 
 describe('with tracked_environments', {
