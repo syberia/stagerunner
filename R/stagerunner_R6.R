fb283117ceefadadaa56f962a83186c84d079f25
RobertZK <technoguyrob@gmail.com>
2015-04-01 22:12:05 -0500

    comment this example out for now

--- R/stagerunner_R6.R
+++ R/stagerunner_R6.R
@@ -455,12 +455,12 @@ stageRunner_append <- function(other_runner, label = NULL) {
 #'
 #' @name stageRunner_stage_names
 #' @return a list of canonical stage names.
-#' @examples
-#' f <- function() {}
-#' sr <- stageRunner$new(new.env(),
-#'   list(a = stageRunner$new(new.env(), list(b = f, c = f)), d = f,
-#'   e = stageRunner$new(new.env(), list(f = f, g = f))))
-#' sr$stage_names()
+## @examples
+## f <- function() {}
+## sr <- stageRunner$new(new.env(),
+##   list(a = stageRunner$new(new.env(), list(b = f, c = f)), d = f,
+##   e = stageRunner$new(new.env(), list(f = f, g = f))))
+## sr$stage_names()
 stageRunner_stage_names <- function() {
   nested_stages <- function(x) if (is.stagerunner(x)) nested_stages(x$stages) else x
   nested_names(lapply(stages, nested_stages))
