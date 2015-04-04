d1c8ec3fb4cb1dfbe136bd29ecc0beb1f9038d42
RobertZK <technoguyrob@gmail.com>
2015-04-03 19:33:39 -0500

    test that a stageRunner list with a NULL can be turned into a stageRunner

--- tests/testthat/test-is_pre_stagerunner.R
+++ tests/testthat/test-is_pre_stagerunner.R
@@ -20,3 +20,8 @@ test_that("a stageRunner list can be turned into a stageRunner", {
   sr <- stageRunner(new.env(), force)
   expect_true(is_pre_stagerunner(list(sr, force)))
 })
+
+test_that("a stageRunner list with a NULL can be turned into a stageRunner", {
+  sr <- stageRunner(new.env(), force)
+  expect_true(is_pre_stagerunner(list(sr, NULL, force)))
+})
