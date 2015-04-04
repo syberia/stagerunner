4651cc95f5ef88321095405b5f63b5f7b63b4b19
RobertZK <technoguyrob@gmail.com>
2015-04-03 19:33:18 -0500

    recursive is_pre_stagerunner checks

--- tests/testthat/test-is_pre_stagerunner.R
+++ tests/testthat/test-is_pre_stagerunner.R
@@ -10,3 +10,13 @@ test_that("a function can be turned into a stageRunner", {
   expect_true(is_pre_stagerunner(force))
   expect_true(is_pre_stagerunner(function(x) x$x <- 1))
 })
+
+test_that("a stageRunner can be turned into a stageRunner", {
+  sr <- stageRunner(new.env(), force)
+  expect_true(is_pre_stagerunner(sr))
+})
+
+test_that("a stageRunner list can be turned into a stageRunner", {
+  sr <- stageRunner(new.env(), force)
+  expect_true(is_pre_stagerunner(list(sr, force)))
+})
