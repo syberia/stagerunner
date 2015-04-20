90e955df25cc4975b9cd307424b1f137c5509038
RobertZK <technoguyrob@gmail.com>
2015-04-20 14:58:16 -0500

    initial rocco docs for boolean_fill

--- R/boolean_fill.r
+++ R/boolean_fill.r
@@ -1,3 +1,14 @@
+## A stagerunner described a *linear* sequence of execution: import data,
+## perform this munging step, then that munging, then do some modeling, etc.
+## However, it is structured hierarchically as a nested list for easier
+## usability. This function will created a nested list with the exact same
+## structure as the stagerunner except that each terminal node is either
+## TRUE or FALSE. 
+##
+## Specifically, given a tree structure with exactly one TRUE value in the
+## terminal nodes, all successors of that node will be marked as TRUE
+## as well. Conversely, if `forward = FALSE`, then all predecessors of
+## that node will be marked as TRUE.
 #' Fill a nested logical list with TRUEs before or after the first TRUE
 #' 
 #' This is a helper function to implement the \code{to} parameter
@@ -11,12 +22,20 @@
 #' @return the filled list
 boolean_fill <- function(el, forward = TRUE) {
   ix <- which(vapply(el, contains_true, logical(1)))[1]
+  ## If no value is TRUE, `which` returns a zero-length vector, and subsetting
+  ## by `[1]` gives `NA`.
   if (!is.finite(ix)) stop("boolean_fill called but no TRUEs found")
-  fills <-
-    if (forward) seq_len(length(el) - ix) + ix
-    else seq_len(ix - 1)
+
+  if (isTRUE(forward)) {
+    fills <- seq_len(length(el) - ix) + ix
+  } else {
+    fills <- seq_len(ix - 1)
+  }
   el[fills] <- TRUE
-  if (!is.atomic(el)) el[[ix]] <- boolean_fill(el[[ix]], forward = forward)
+  
+  if (!is.atomic(el[[ix]])) {
+    el[[ix]] <- boolean_fill(el[[ix]], forward = forward)
+  }
   el
 }
 
