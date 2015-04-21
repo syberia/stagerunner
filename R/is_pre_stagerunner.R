20252b6dcc54a81a09b4fbabc7f1c499a91bd52d
RobertZK <technoguyrob@gmail.com>
2015-04-21 17:35:50 -0500

    rocco docs for is_pre_stagerunner

--- R/is_pre_stagerunner.R
+++ R/is_pre_stagerunner.R
@@ -1,3 +1,17 @@
+## The only way to turn an object into a stagerunner is if it can be
+## interpreted as a hierarchical sequence of execution: run some stuff
+## in group A, then run some stuff in group B, and so on, with each
+## group potentially containing more subgroups.
+##
+## In other words, the things which can be turned into stagerunners
+## are:
+## 
+##    * functions
+##    * other stagerunners (sub-stagerunners)
+##    * lists composed of the above
+##
+## The purpose of the `is_pre_stagerunner` function is to determine
+## whether an object satisfies these restrictions.
 #' Whether or not an object can be transformed into a stageRunner.
 #'
 #' @param x ANY. An R object for which it will be determined whether or not
@@ -20,7 +34,11 @@ is_pre_stagerunner <- function(x) {
   if (is.function(x) || is.stagerunner(x)) { return(TRUE) }
   if (!is.recursive(x) || is.environment(x)) { return(FALSE) }
 
+  ## Using a for loop is a tiny bit faster than a apply-family operation
+  ## because we can exit the function early.
   for (i in seq_along(x)) {
+    ## We use the base function [`Recall`](https://stat.ethz.ch/R-manual/R-devel/library/base/html/Recall.html)
+    ## for its recursive effect.
     if (!(is.function(x[[i]]) || is.stagerunner(x[[i]]) || is.null(x[[i]]) ||
           (is.recursive(x[[i]]) && Recall(x[[i]])))) {
       return(FALSE)
