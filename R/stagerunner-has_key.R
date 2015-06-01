7347c6345d8431a2ce38cc08623f11d176268f40
RobertZK <technoguyrob@gmail.com>
2015-06-01 10:00:52 -0500

    Rocco docs for has_key method

--- R/stagerunner-has_key.R
+++ R/stagerunner-has_key.R
@@ -3,7 +3,15 @@
 #' @param key ANY. The potential key.
 #' @return \code{TRUE} or \code{FALSE} accordingly.
 stageRunner_has_key <- function(key) {
-  has <- tryCatch(normalize_stage_keys(key, self$stages), error = function(.) FALSE)
+  ## We turn the key, like "data/foo" or `c(1,2)` into a
+  ## nested list of logicals in the usual format, or `FALSE` if
+  ## the key cannot be parsed.
+  has <- tryCatch(normalize_stage_keys(key, self$stages),
+                  error = function(.) FALSE)
+  ## If any substage is `TRUE`, the stagerunner contains this key.
+  ## Note that keys may refer to several different substages!
+  ## This method will tell us whether it is possible to execute anything
+  ## using the provided `key`.
   any(c(has, recursive = TRUE))
 }
 
