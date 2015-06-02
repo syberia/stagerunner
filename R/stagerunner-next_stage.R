5eea052b97f4689382e3e9f4e18da28a3796d30c
RobertZK <technoguyrob@gmail.com>
2015-06-02 15:13:25 -0500

    Rocco documentation for stageRunner

--- R/stagerunner-next_stage.R
+++ R/stagerunner-next_stage.R
@@ -1,3 +1,9 @@
+## Stagerunners allow for the option to use a special mode called `"next"`. In
+## this mode, instead of executing by default from the beginning of the
+## stagerunner, execution will commence from the last non-executed stage.
+##
+## This allows us to repeatedly call `runner$run()` until it has finished
+## executing, if errors occur during the process and we repeatedly fix them.
 #' For stageRunners with caching, find the next unexecuted stage.
 #'
 #' @name stageRunner_next_stage
@@ -7,14 +13,18 @@
 #'   always return the first stage key (`'1'`).
 stageRunner_next_stage <- function() {
   for (stage_index in seq_along(self$stages)) {
+    ## We use the `stageRunnerNode$was_executed` helper to determine if this
+    ## stage has been executed yet.
     is_unexecuted_terminal_node <- is.stageRunnerNode(self$stages[[stage_index]]) &&
       !self$stages[[stage_index]]$was_executed()
+    if (is_unexecuted_terminal_node) return(as.character(stage_index))
+
+    ## We can recursively use `next_stage` if the current stage is another
+    ## stagerunner rather than a terminal node.
     has_unexecuted_terminal_node <- is.stagerunner(self$stages[[stage_index]]) &&
       is.character(tmp <- self$stages[[stage_index]]$next_stage())
 
-    if (is_unexecuted_terminal_node) return(as.character(stage_index))
-    else if (has_unexecuted_terminal_node)
-      return(paste(c(stage_index, tmp), collapse = '/'))
+    if (has_unexecuted_terminal_node) return(paste(c(stage_index, tmp), collapse = '/'))
   }
   FALSE
 }
