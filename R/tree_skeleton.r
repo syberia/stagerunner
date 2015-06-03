7f1cd12b788c55a6926f6bd69b022b7357b8ef9e
RobertZK <technoguyrob@gmail.com>
2015-06-02 21:57:02 -0500

    Move treeSkeleton to its own file

--- R/tree_skeleton.r
+++ R/tree_skeleton.r
@@ -47,22 +47,6 @@ treeSkeleton__children <- function() {
                        parent_caller = self$parent_caller)
 }
 
-#' Find the index of the current object in the children of its parent.
-#' @name treeSkeleton__.parent_index
-treeSkeleton__.parent_index <- function() {
-  if (!is.null(ci <- attr(self$object, 'child_index'))) ci
-  # Hack for accessing attribute modifications on a reference class object
-  # See: http://stackoverflow.com/questions/22752021/why-is-r-capricious-in-its-use-of-attributes-on-reference-class-objects
-  else if (is.refClass(self$object) &&
-           !is.null(ci <- attr(attr(self$object, '.xData')$.self, 'child_index'))) ci
-  else # look through the parent's children and compare to .self
-    # Danger Will Robinson! This will lead to strange bugs if our tree
-    # has several nodes with duplicate objects
-    which(vapply(
-      self$parent()$children(),
-      function(node) identical(node$object, self$object), logical(1)))[1]
-}
-
 #' Find the key with the given index using the names of the lists
 #' that parametrize each node's children.
 #'
