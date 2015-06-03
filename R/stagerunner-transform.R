9d4ade5aeb90c0f525d2a8eb239a8f5310b2da9a
RobertZK <technoguyrob@gmail.com>
2015-06-02 21:06:33 -0500

    Literately document stagerunner method

--- R/stagerunner-transform.R
+++ R/stagerunner-transform.R
@@ -1,3 +1,17 @@
+## Straightforwardly, apply some function (`transformation`)
+## to every terminal node in the stagerunner. This is useful for
+## simple debugging and monitoring. For example, if we wish to 
+## print the variables currently in the context of stagerunner
+## prior to executing each stage, we can call
+##
+## ```r
+## runner$transform(function(fn) {
+##   function(context, ...) {
+##     print(ls(context))
+##     fn(context, ...)
+##   }
+## })
+## ```
 #' Transform the callable's of the terminal nodes of a stageRunner.
 #'
 #' Every terminal node in a stageRunner is of type stageRunnerNode.
@@ -8,7 +22,10 @@
 #' @param transformation function. The function which transforms one callable
 #'   into another.
 stageRunner_transform <- function(transformation) {
-  for (stage_index in seq_along(self$stages))
+  for (stage_index in seq_along(self$stages)) {
     self$stages[[stage_index]]$transform(transformation)
+  }
+  self
 }
+
 
