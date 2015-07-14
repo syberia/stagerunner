eaf5853f81538bee2765f91be67ea8d4e383ea07
RobertZK <technoguyrob@gmail.com>
2015-07-14 16:48:43 -0500

    Test ordinal printing in verbose runs

--- tests/testthat/test-stage_runner__verbose.R
+++ tests/testthat/test-stage_runner__verbose.R
@@ -2,9 +2,11 @@ context("verbose runs")
 
 test_that("it can run verbosely for a simple stagerunner", {
   sr <- stageRunner$new(new.env(),
-    list(a = force, b = list(c = force, d = force)))
+    list(a = force, b = list(c = force, d = force), force))
   expect_output(sr$run(verbose = TRUE), "Running 1.*a.*\\.\\.\\.")
   expect_output(sr$run(verbose = TRUE), "Beginning 2.*b.*stage\\.\\.\\.")
   expect_output(sr$run(verbose = TRUE), "Running 1.*c.*\\.\\.\\.")
   expect_output(sr$run(verbose = TRUE), "Ending 2.*b.*stage\\.\\.\\.")
+  expect_output(sr$run(verbose = TRUE), "Running 3.*third.*")
 })
+
