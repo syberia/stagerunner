81f448d0267a9cf784fea4130b65a341525b5757
RobertZK <technoguyrob@gmail.com>
2015-07-14 11:21:29 -0500

    check that has_key works on characters

--- tests/testthat/test-stage_runner__has_key.R
+++ tests/testthat/test-stage_runner__has_key.R
@@ -1,5 +1,14 @@
 context('stagerunner$has_key')
 
-test_that("it tells when a stagerunner does no have a key", {
+test_that("it tells when a stagerunner does not have a key", {
   expect_false(stagerunner(new.env(), list("foo" = force))$has_key("bar"))
 })
+
+test_that("it tells when a stagerunner has a key", {
+  expect_true(stagerunner(new.env(), list("foo" = force))$has_key("foo"))
+})
+
+test_that("it tells when a stagerunner has a partial matching key", {
+  expect_true(stagerunner(new.env(), list("foo" = list("bar" = force)))$has_key("f/ba"))
+})
+
