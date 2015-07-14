4841a5783f27a8e478b5d6f4efc5a950719ab08c
RobertZK <technoguyrob@gmail.com>
2015-07-14 11:22:09 -0500

    check has_key on numeric inputs

--- tests/testthat/test-stage_runner__has_key.R
+++ tests/testthat/test-stage_runner__has_key.R
@@ -12,3 +12,11 @@ test_that("it tells when a stagerunner has a partial matching key", {
   expect_true(stagerunner(new.env(), list("foo" = list("bar" = force)))$has_key("f/ba"))
 })
 
+test_that("it tells when a stagerunner has a partial matching key numerically given", {
+  expect_true(stagerunner(new.env(), list("foo" = list("bar" = force, "baz" = force)))$has_key("1/2"))
+})
+
+test_that("it tells when a stagerunner has a partial matching key numerically given", {
+  expect_false(stagerunner(new.env(), list("foo" = list("bar" = force, "baz" = force)))$has_key("1/3"))
+})
+
