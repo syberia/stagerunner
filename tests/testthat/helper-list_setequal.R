c7581ea6f994a48c3635829c350cbbe43c37c4fd
RobertZK <technoguyrob@gmail.com>
2016-04-20 17:24:59 -0500

    Add list_setequal helper.

---
+++ tests/testthat/helper-list_setequal.R
@@ -0,0 +1,14 @@
+#' Test that two lists are the same as named sets.
+#'
+#' @param list1 list.
+#' @param list2 list.
+#' @return If the lists are the same as named sets.
+list_setequal <- function(list1, list2) {
+  if (is.null(names(list1)) || is.null(names(list2))) {
+    stop("Only named lists can be compared using list_setequal.")
+  }
+  stopifnot(all(nzchar(list1)), all(nzchar(list2)))
+  stopifnot(setequal(names(list1), names(list2)))
+  isTRUE(all.equal(list1, list2[names(list1)] ))
+}
+
